8d8dd349c8da5fcad40665e351b062c954fb9c10
afu <yulifu@douyu.tv>
2024-04-19 11:14:23 +0800

    change entry file

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const CandyJs = require('candyjs');
 const Candy = require('candyjs/Candy');
 const App = require('candyjs/web/Application');
 
-Candy.setPathAlias('@n_m', __dirname + '/node_modules');
+Candy.setPathAlias('@npm', __dirname + '/node_modules');
 
 new CandyJs(new App({
     'id': 1,
     'debug': true,
     'appPath': __dirname + '/app',
-    // 使用别名路径指定使用的模板引擎
-    'defaultView': 'n_m/@candyjs/template-hbs/index'
+    // alias path of template engine
+    'defaultView': 'npm/@candyjs/template-hbs/index'
 
 })).listen(2333, function(){
     console.log('listen on 2333');
